68983ae2ed7072e6eae54bd0fcc06f7fde5b5e5e
Dennis111111 <87600478+Dennis111111@users.noreply.github.com>
2022-04-11 16:13:26 +0800

    Add files via upload

---
+++ stanCode_SC001_Project/MoveToTheEnd.py
@@ -0,0 +1,63 @@
+"""
+File: MoveToTheEnd.py
+Name: Jerry Liao
+------------------------
+This file shows how to use while loop
+to walk to the end of a certain row in
+karel world
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
